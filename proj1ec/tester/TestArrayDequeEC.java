d9189f9cc2638c6ed32f0da34f69d8f97b630bb3
Yutong Wang <aeo9539@gmail.com>
2024-01-27 21:31:48 -0500

    changed random implementation

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -7,44 +7,40 @@ import edu.princeton.cs.algs4.StdRandom;
 import student.StudentArrayDeque;
 
 public class TestArrayDequeEC {
-    StudentArrayDeque<Integer> student = new StudentArrayDeque<>();
-    ArrayDequeSolution<Integer> expected = new ArrayDequeSolution<>();
 
     @Test
     public void randomizedTest() {
+        StudentArrayDeque<Integer> student = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> expected = new ArrayDequeSolution<>();
+
         int total = 100;
         for (int i = 0; i < total; i++) {
-            double randomOperation = StdRandom.uniform();
             StringBuilder errorMsg = new StringBuilder();
             errorMsg.append("\n");
 
-            int randomTests = StdRandom.uniform(0, 10);
-            for (int j = 1; j < randomTests; j++) {
-                int randomNumber = StdRandom.uniform(0, 1000);
-                student.addFirst(randomNumber);
-                expected.addFirst(randomNumber);
-                errorMsg.append(String.format("addFirst(%s)\n", randomNumber));
-
-                randomNumber = StdRandom.uniform(0, 1000);
-                student.addLast(randomNumber);
-                expected.addLast(randomNumber);
-                errorMsg.append(String.format("addLast(%s)\n", randomNumber));
-            }
-            assertEquals(errorMsg.toString(), expected.size(), student.size());
-
-            for (int k = 1; k < randomTests; k++) {
-                if (!(student.isEmpty()) && !(expected.isEmpty())) {
-                    Integer studentItem;
-                    Integer expectedItem;
-                    if (randomOperation < 0.5) {
-                        studentItem = student.removeFirst();
-                        expectedItem = expected.removeFirst();
-                        errorMsg.append(String.format("removeFirst(): %s\n", expectedItem));
-                    } else {
-                        studentItem = student.removeLast();
-                        expectedItem = expected.removeLast();
-                        errorMsg.append(String.format("removeLast(): %s\n", expectedItem));
-                    }
+            for (int j = 0; j < 5; j++) {
+                int randomOperation = StdRandom.uniform(0, 6);
+                if (randomOperation < 2) {
+                    int randomNumber = StdRandom.uniform(0, 1000);
+                    student.addFirst(randomNumber);
+                    expected.addFirst(randomNumber);
+                    errorMsg.append(String.format("addFirst(%s)\n", randomNumber));
+                    assertEquals(errorMsg.toString(), expected.size(), student.size());
+                } else if (randomOperation < 4) {
+                    int randomNumber = StdRandom.uniform(0, 1000);
+                    student.addLast(randomNumber);
+                    expected.addLast(randomNumber);
+                    errorMsg.append(String.format("addLast(%s)\n", randomNumber));
+                    assertEquals(errorMsg.toString(), expected.size(), student.size());
+                } else if (randomOperation == 4 && !(student.isEmpty()) && !(expected.isEmpty())) {
+                    Integer studentItem = student.removeFirst();
+                    Integer expectedItem = expected.removeFirst();
+                    errorMsg.append(String.format("removeFirst(): %s\n", expectedItem));
+                    assertEquals(errorMsg.toString(), expectedItem, studentItem);
+                } else if (randomOperation == 5 && !(student.isEmpty()) && !(expected.isEmpty())) {
+                    Integer studentItem = student.removeLast();
+                    Integer expectedItem = expected.removeLast();
+                    errorMsg.append(String.format("removeLast(): %s\n", expectedItem));
                     assertEquals(errorMsg.toString(), expectedItem, studentItem);
                 }
             }
