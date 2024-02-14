8f19350cbe54386bdc581778755ed4f28da8aa47
Yutong Wang <aeo9539@gmail.com>
2024-02-14 14:15:31 -0500

    partly finished

--- lab8/hashmap/Map61B.java
+++ lab8/hashmap/Map61B.java
@@ -1,13 +1,14 @@
 package hashmap;
 
 import java.util.Set;
+
 /**
  * Your implementation hashmap.MyHashMap should implement this interface. To do so,
  * append "implements hashmap.Map61B<K, V>" to the end of your "public class..."
  * declaration, though you can use other formal type parameters if you'd like.
  */
 public interface Map61B<K, V> extends Iterable<K> {
-    /** Removes all of the mappings from this map. */
+    /** Removes all the mappings from this map. */
     void clear();
 
     /** Returns true if this map contains a mapping for the specified key. */
